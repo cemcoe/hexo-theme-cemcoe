613df1c414874da7dd0c3d60bfe7d266f3374986
cemcoe <chemxcode@gmail.com>
2020-03-19 19:01:13 +0800

    目录功能

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,19 @@
 console.log('index')
 
 const openChapterBtn = document.querySelector('.open-chapter')
+
+
+const bookChapters = document.querySelector('#book-chapters')
 openChapterBtn.addEventListener('click', () => {
     console.log('click')
+    bookChapters.style.display = "block"
 })
+
+const post = document.querySelector(".post")
+post.addEventListener('click', () => {
+    bookChapters.style.display = "none"
+})
+
+
+
+
