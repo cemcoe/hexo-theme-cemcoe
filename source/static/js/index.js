792d19375bf594e44db321bb6f0eff3bdd63381e
cemcoe <chemxcode@gmail.com>
2020-03-21 19:45:01 +0800

    目录功能

--- source/static/js/index.js
+++ source/static/js/index.js
@@ -1,18 +1,30 @@
 console.log('index')
 
 const openChapterBtn = document.querySelector('.open-chapter')
+const body = document.querySelector("body")
+
+const backdrop = document.querySelector("#backdrop")
+backdrop.addEventListener('click', () => {
+    bookChapters.style.display = "none"
+    backdrop.style.display = "none"
+    body.style.overflowY = "auto"
+})
 
 
 const bookChapters = document.querySelector('#book-chapters')
 openChapterBtn.addEventListener('click', () => {
     console.log('click')
     bookChapters.style.display = "block"
+    backdrop.style.display = "block"
+    body.style.overflowY = "hidden"
+    
 })
 
-const post = document.querySelector(".post")
-post.addEventListener('click', () => {
-    bookChapters.style.display = "none"
-})
+// const post = document.querySelector(".post")
+
+// 禁用文章滚动条
+
+
 
 
 
